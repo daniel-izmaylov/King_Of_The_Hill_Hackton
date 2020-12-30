edf5325f2e6ea1b871047b469c8cfcda1c4fe9ff
ofekron33 <ofekron33@gmail.com>
2020-12-30 18:27:27 +0200

    client Server

--- Client.py
+++ Client.py
@@ -20,17 +20,23 @@ class Client():
             # if (b=="Y"):
             #
 
-    def open_udp_client( self ):
-        broadSock = socket(AF_INET, SOCK_DGRAM)
+    def open_udp_client(self):
+        try:
+            broadSock = socket(AF_INET, SOCK_DGRAM)
+        except socket.error:
+            print ("Error creating socket: %s.creating a new one" %  socket.error)
         broadSock.setsockopt(SOL_SOCKET, SO_BROADCAST, 1) #SO_BROADCAST used to protectet the application from accidentally sending a datagram to many systems
         broadData = 8000
         broadSock.bind(('', 33333))
         while True:
             print("Looking for someone to play with")
-            data, address = broadSock.recvfrom(1024)
+            try:
+                data, address = broadSock.recvfrom(1024)
+            except:
+                pass
             return int((data.decode()))
 
-    def open_tcp_client( self, port=13117, team_name="X Æ A-12" ):
+    def open_tcp_client( self, port=13117, team_name="A" ):
         def in_game():
             with Listener(
                     on_press=on_press,
@@ -44,21 +50,34 @@ class Client():
             print('{0} pressed'.format(
                 key))
             print("in")
-            clientSocket.send(str(key).encode())
-            print("out")
+            i=0
+            try:
+                clientSocket.send(str(key).encode())
+                print("out")
+                if (str(key) not in Pressed_keys):
+                    Pressed_keys[str(key)] = 0
+                Pressed_keys[str(key)] += 1
+            except:
+                pass
 
-            if (str(key) not in Pressed_keys):
-                Pressed_keys[str(key)] = 0
-            Pressed_keys[str(key)] += 1
 
         print("Trying to connect to server started...")
         print("******************")
         print("Messages are traveling in light speed to make this game work")
         print("******************")
         Pressed_keys = {}
+        counter=0
         server_address = (('127.0.0.1', port))
         # server_address=('127.1.0.4',13117)
-        clientSocket = socket(AF_INET, SOCK_STREAM)
+        while True:
+            try:
+                clientSocket = socket(AF_INET, SOCK_STREAM)
+                break
+            except :
+                print("error creating socket. trying again")
+                counter+=1
+                if(counter==10):
+                    print("starting a new game")
         connected = False
         i = 0
         while not connected:
@@ -75,12 +94,21 @@ class Client():
 
         print("Connected to server address: ", str(server_address))
         sentence = str.encode(team_name + "\n")
-        clientSocket.send(sentence)
+        counter=0
+        while True:
+            try:
+                clientSocket.send(sentence)
+                break
+            except:
+                counter+=1
+                if(counter==10):
+                    print("error sending team name,starting a new game")
+                    return
         try: #TODO: TRY ALLLL
             recieve_from_server = clientSocket.recv(1024)  ##port 1024 is for tcp
-        except ConnectionResetError:
-            print('Opps.... looks like the host disconcted')
-            return
+        except timeout:
+                print('Opps.... looks like the host disconcted')
+                return
         print(recieve_from_server.decode())
         start_time = time.time()
         clientSocket.settimeout(10)
@@ -88,20 +116,26 @@ class Client():
             in_game()
         except timeout:
             print("\n-=Times Up=-\n")
-
+        except Exception as e:
+            print(e)
+            return
         print("Good Job you Pressed {} Keys this game.".format(sum(Pressed_keys.values())))
         print("The most frequent key pressed was:", max(Pressed_keys, key=lambda k: Pressed_keys[k]))
         print("Waiting for Results...")
         clientSocket.settimeout(10)
 
-        while (True):
+        print("waiting for game results.")
+        try:
             recieve_from_server = clientSocket.recv(1024)
-            if (recieve_from_server != ""):
-                print(recieve_from_server.decode())
-                break
-            print("Stuck")
+        except socket.error:
+            print("looks like the game results got lost somewhere..lets play again")
+            return
 
-        return
+        if (recieve_from_server != ""):
+            print(recieve_from_server.decode())
+
+
+
 
 
 if __name__ == '__main__':
